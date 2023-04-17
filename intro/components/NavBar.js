bdb82a239f1039f4a9d9657b71964f181a3182b2
이진형 <dlwlsgud2416@naver.com>
2023-04-17 10:02:57 +0000

    0

--- intro/components/NavBar.js
+++ intro/components/NavBar.js
@@ -1,8 +1,24 @@
-export default function NavVar() {
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+    const router = useRouter();
     return (
         <nav>
-            <a herf="/">Home</a>
-            <a herf="/about">About</a>
+            <Link href="/">
+                Home
+            </Link>
+            <Link href="/about">
+                About
+            </Link>
+            <style jsx>{`
+            nav{
+                background-color:tomato;
+            }
+            nav{
+                text-decoration:none;
+            }
+            `}</style>
         </nav>
     );
 }
